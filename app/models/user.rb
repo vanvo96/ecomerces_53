6117bf0b1ef1fa4d3053f2e5cbdc832d0f86fde7
vanvo96 <vo.trong.van@framgia.com>
2018-08-31 15:12:04 +0700

    account_new

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,28 @@
 class User < ApplicationRecord
-  has_secure_password
   has_many :orders
   has_many :suggests
   has_many :comments
   has_many :ratings
+
+  before_save{email.downcase!}
+  validates :name, presence: true,
+    length: {maximum: Settings.user.name.length.maximum}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+    length: {maximum: Settings.user.email.length.maximum},
+    format: {with: VALID_EMAIL_REGEX},
+    uniqueness: {case_sensitive: false}
+  has_secure_password
+  validates :password, presence: true,
+    length: {minimum: Settings.user.password.length.minimum}
+  validates :phone, presence: true
+
+  def self.digest string
+    cost =  if ActiveModel::SecurePassword.min_cost
+              BCrypt::Engine::MIN_COST
+            else
+              BCrypt::Engine.cost
+            end
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
