dfecee4660775a24d6743e56321ae4b027a91d95
vanvo96 <vo.trong.van@framgia.com>
2018-09-04 13:51:02 +0700

    Top Trends

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,19 @@ class Product < ApplicationRecord
   has_many :comments
   has_many :ratings
   has_many :images
+
+  scope :top_trends, (lambda do
+    select("products.id, products.price, products.name, images.url,
+     sum(order_details.quantity) as order_max")
+    .joins(:order_details, :images)
+    .group(:id)
+    .limit(12)
+  end)
+
+  # def self.top_trend
+  #   Product.joins(:order_details)
+  #   .select("products.id, Sum(order_details.quantity) as tong ")
+  #   .group("products.id")
+  #   .limit(12)
+  # end
 end
